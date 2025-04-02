68ba34aac63a41302d2153cc8c783c8ffa686b89
ColleagueRiley <ColleagueRiley@gmail.com>
2025-04-02 19:43:13 -0400

    update

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,7 +6,10 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+
 map("n", "<leader>e", "<cmd>NvimTreeToggle<cr>", { desc = "Toggle File Explorer" })
 
-map("n", "<leader>v", "<cmd>ToggleTerm<cr>", { desc = "Toggle Terminal" })
+map("n", "<leader>db", "<cmd>DapToggleBreakpoint <cr>", { desc = "Add breakpoint at line" })
+map("n", "<leader>dr", "<cmd>DapContiune <cr>", { desc = "Start or continue the debugger" })
 --  map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+--
