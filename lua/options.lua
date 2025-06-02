cd3ff128ac3d3e8cd714c986b83681be67322f91
ColleagueRiley <ColleagueRiley@gmail.com>
2025-06-01 21:34:42 -0400

    update lua options, for tab spacing

--- lua/options.lua
+++ lua/options.lua
@@ -7,6 +7,8 @@ require "nvchad.options"
 vim.cmd("set tabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set softtabstop=4")
+vim.cmd("set softtabstop=4")
 vim.cmd("set relativenumber")
+vim.cmd("set expandtab = true");
 
 vim.o.clipboard = "unnamedplus"
